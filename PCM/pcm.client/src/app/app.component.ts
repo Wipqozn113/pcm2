efd00ca6009304b61c6339e7975ef989e3bb7217
Bryan Cave <bryan.d.cave@gmail.com>
2024-06-03 15:36:31 -0300

    Update app.component.ts

--- PCM/pcm.client/src/app/app.component.ts
+++ PCM/pcm.client/src/app/app.component.ts
@@ -49,19 +49,5 @@ export class AppComponent implements OnInit {
     );
   }
 
-  copyMessage(val: string) {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
-    selBox.value = val;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    document.execCommand('copy');
-    document.body.removeChild(selBox);
-  }
-
   title = 'pcm.client';
 }
